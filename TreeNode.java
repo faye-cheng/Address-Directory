039a396bab80007f7851f373081bee54688db4d2
Faye Cheng <fayehcheng@gmail.com>
2015-10-20 10:07:45 -0700

    Create TreeNode.java

---
+++ TreeNode.java
@@ -0,0 +1,13 @@
+package for_databaseFA;
+
+public class TreeNode <E>{
+	E myData;//reference to the data
+	TreeNode<E> myLeft;//reference to the left Node
+	TreeNode<E> myRight;//reference to the right Node
+
+	public TreeNode(E theData){
+		myLeft= null;
+		myRight= null;
+	}//1 parameter constructor
+
+}//Node
